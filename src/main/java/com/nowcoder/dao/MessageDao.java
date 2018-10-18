9ac4e63e4d64218819dd522d139658939c1eb35b
刘懿庆 <liuyq9@asiainfo.com>
2018-10-18 10:14:19 +0800

    发送消息

---
+++ src/main/java/com/nowcoder/dao/MessageDao.java
@@ -0,0 +1,45 @@
+package com.nowcoder.dao;
+
+import com.nowcoder.model.Message;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @Author: miaomiao
+ * @Date: 2018/10/17
+ * @Description:
+ **/
+@Mapper
+@Repository
+public interface MessageDao {
+
+    String TABLE_NAME = " message ";
+    String INSERT_FIELDS = " from_id,to_id,content,has_read,conversation_id,created_date ";
+    String SELECT_FIELDS = "id" + INSERT_FIELDS;
+
+    @Insert({"insert into ",TABLE_NAME, " ( ",INSERT_FIELDS, " ) values ( #{fromId},#{toId},#{content},#{hasRead},#{conversationId},#{createdDate} )"})
+    int addMessage(Message message);
+
+
+    @Select({"select ",SELECT_FIELDS, "from ",TABLE_NAME,
+            "where conversation_id = #{conversationId} order by created_date desc limit #{offset},#{limit}"})
+    List<Message> selectConversationDetail(@Param("conversationId") String conversationId,
+                                           @Param("offset") int offset,
+                                           @Param("limit") int limit);
+
+    @Select({"select",INSERT_FIELDS,",count(*) as id from ( select * from", TABLE_NAME ,
+            "where from_id = #{userId} or to_id = #{userId} order by created_date desc) t " +
+                    "group by conversation_id order by created_date desc limit #{offset},#{limit}"})
+    List<Message> selectConversationList(@Param("userId") int userId,
+                                               @Param("offset") int offset,
+                                               @Param("limit") int limit);
+
+    @Select({"select count(id) from",TABLE_NAME,"where has_read = 0 and to_id = #{userId} and conversation_id = #{conversationId}"})
+    int getConversationUnreadCount(@Param("conversationId") String conversationId,
+                                   @Param("userId") int userId);
+
+    @Update({"update",TABLE_NAME,"set has_read = 1 where conversation_id = #{conversationId}"})
+    int updateConversationRead(String conversationId);
+}
---
+++ src/main/java/com/nowcoder/service/MessageService.java
@@ -0,0 +1,44 @@
+package com.nowcoder.service;
+
+import com.nowcoder.dao.MessageDao;
+import com.nowcoder.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Author: miaomiao
+ * @Date: 2018/10/17
+ * @Description:
+ **/
+@Service
+public class MessageService {
+
+    @Autowired
+    MessageDao messageDao;
+
+    @Autowired
+    SensitiveService sensitiveService;
+
+    public int addMessage(Message message) {
+        message.setContent(sensitiveService.filter(message.getContent()));
+        return messageDao.addMessage(message) > 0 ? message.getId() : 0;
+    }
+
+    public List<Message> getConversationDetail(String conversationId,int offset,int limit){
+        return messageDao.selectConversationDetail(conversationId,offset,limit);
+    }
+
+    public List<Message> getConversationList(int userId,int offset,int limit){
+        return messageDao.selectConversationList(userId, offset, limit);
+    }
+
+    public int getConversationUnreadCount(int userId,String conversationId){
+        return messageDao.getConversationUnreadCount(conversationId,userId);
+    }
+
+    public int updateConversationRead(String conversationId){
+        return messageDao.updateConversationRead(conversationId);
+    }
+}
